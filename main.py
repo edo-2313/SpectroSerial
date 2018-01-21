9aa84169da37556938cde3b689af6318152459dc
edo-2313 <edo2313@gmail.com>
2018-01-21 19:47:39 +0100

    added setup

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-from openpyxl import Workbook
+from openpyxl import Workbook   #modulo per manipolare file excel
+from openpyxl.chart import (    #moduli per creare grafici su excel
+    LineChart,
+    Reference,
+)
 
+import serial
 
-wb = Workbook()
-ws= wb.active
-#ws['A3']=142
-#ws['B7']=0.28
-for a in range(1,10):
-    for b in range(1,10):
-        ws.cell(row=a, column=b, value=a+b)
-wb.save('test.xlsx')
+ster='Banana'
+print(ster)
+ser = serial.Serial('COM7')  # open serial port
+ser.write(bytes(ster, 'utf-8'))
+while True:
+    btr = ser.inWaiting()
+    data = ser.    print(data)
+#wb = Workbook()
+#ws= wb.active
+#for a in range(1,10):
+#    for b in range(1,10):
+#        ws.cell(row=a, column=b, value=a+b)
+#wb.save('dati.xlsx')
